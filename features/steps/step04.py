d7aef51b23c630bd920f1e3db77551053c65587e
DEV-COM <kwp@tlen.pl>
2024-08-10 14:59:46 +0200

    6

--- features/steps/step04.py
+++ features/steps/step04.py
@@ -2,7 +2,6 @@ from behave import given, when, then
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 @given('wchodzimy na strone madison island')

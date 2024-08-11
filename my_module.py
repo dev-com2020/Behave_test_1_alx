80ce4984c18a3cd022591d2d746c8eab4a9fa433
DEV-COM <kwp@tlen.pl>
2024-08-11 16:24:31 +0200

    7

---
+++ my_module.py
@@ -0,0 +1,38 @@
+def add(x, y):
+    """
+    Dodaje dwie liczy
+    :param x: (int)liczba pierwsza
+    :param y: (int)liczba druga
+    :return:  (int)sumę obu liczb
+
+    Example:
+    add(2,3)
+    5
+    add(-2,5)
+    3
+    """
+    return x + y
+
+
+def multiply(x, y):
+    """
+    mnożymy dwie liczby
+    :param x: (int)liczba pierwsza
+    :param y: (int)liczba druga
+    :return: (int)wynik obu liczb
+
+    Example:
+    multiply(2,2)
+    4
+    """
+    return x * y
+
+
+def odejmowanie(x, y):
+    """
+
+    :param x:
+    :param y:
+    :return:
+    """
+    return x - y
---
+++ tests/test_add.py
@@ -0,0 +1,27 @@
+from my_module import add, multiply, odejmowanie
+
+
+def test_add():
+    """
+    Przypadek testowy dla dokumentacji funkcji add()
+    :return:
+    """
+    assert add(2, 3) == 5
+    assert add(-2, 5) == 3
+
+
+def test_multi():
+    """
+    Przypadek testowy dla dokumentacji funkcji multiply()
+    :return:
+    """
+    assert multiply(2, 2) == 4
+
+
+
+def test_odejmowanie():
+    """
+    Przypadek testowy dla dokumentacji funkcji odejmowanie()
+    :return:
+    """
+    assert odejmowanie(10, 5) == 5
